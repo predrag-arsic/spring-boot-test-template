7caaf1de8e159bdabec9cfb29c783bfb45e18397
predrag-arsic <predrag87arsic@gmail.com>
2024-11-13 13:26:29 +0100

    MockMvcResultMatchers has number of utilitu matchers

--- productservice/src/test/java/com/globomantics/productservice/web/ProductControllerTest.java
+++ productservice/src/test/java/com/globomantics/productservice/web/ProductControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
